8152146ca5b4d8a21e8cbe7d0e3333e74087ce18
Chaitanya2308 <35360720+Chaitanya2308@users.noreply.github.com>
2021-01-06 09:55:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+ 
+setTimeout(function () {
+    console.log('10')
+    setTimeout(function () {
+        console.log('9')
+            setTimeout(function () {
+                console.log('8')
+                setTimeout(function () {
+                    console.log('7')
+                    setTimeout(function () {
+                        console.log('6')
+                        setTimeout(function () {
+                            console.log('5')
+                            setTimeout(function () {
+                                console.log('4')
+                                setTimeout(function () {
+                                    console.log('3')
+                                    setTimeout(function () {
+                                        console.log('2')
+                                        setTimeout(function () {
+                                            console.log('1')
+                                            setTimeout(function () {
+                                                console.log('Happy New Year 2021')
+                                                
+                                            },1000)
+                                            
+                                        },1000)
+                                        
+                                    },1000)
+                                    
+                                },1000)
+                                
+                            },1000)
+                            
+                        },1000)
+                        
+                    },1000)
+                    
+                },1000)
+                
+            },1000)
+    },1000)
+},1000)
+
+
